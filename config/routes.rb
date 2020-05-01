15bb30351e16db026ff670889a10fe578e67e082
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-05-01 21:29:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+  
+  get '/coupons', to: 'coupons#index'
+  post '/coupons', to: 'coupons#create'
+  get '/coupons/new', to: 'coupons#new', as: 'new_coupon'
+  get '/coupons/:id', to: 'coupons#show', as: 'coupon'
+
